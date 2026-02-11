b765be49f9498ae99d97783b3829a48e5aa159b5
Damoon Saber <damoonsaber@gmail.com>
2026-02-11 23:35:11 +0330

    adding constants

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -7,19 +7,36 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ChatViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
+    var messages : [Message] = [
+        Message(sender: "1@2.com", body: "hey"),
+        Message(sender: "a@b.com", body: "whats up"),
+        Message(sender: "1@2.com", body: "hello")
+    ]
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        title = K.appName
+        navigationItem.hidesBackButton = true
 
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
     }
     
-
+    @IBAction func logOutPressed(_ sender: UIBarButtonItem) {
+        do {
+            try Auth.auth().signOut()
+            navigationController?.popToRootViewController(animated: true)
+        } catch let signOutError as NSError {
+          print("Error signing out: %@", signOutError)
+        }
+    }
+    
 }
---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Damoon saber on 11/22/1404 AP.
+//  Copyright © 1404 AP Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
